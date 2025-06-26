767fe90ce0daa897a21fa210b33fa9d8036acf5b
KarmoussaCode <Karmoussacode@gmail.com>
2025-06-26 10:36:14 +0100

    fix:message demande devis not working only on netlify

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -145,6 +145,30 @@ function revealOnScroll() {
         }
     });
 }
+// Fonction globale qu'on appelle depuis le HTML
+function demanderDevis(nomProduit, materiau, capacite, conditionnement) {
+    const message = `🔹 Bonjour, je suis intéressé par le produit ${nomProduit}.
+  Matière: ${materiau}
+  Capacité: ${capacite}
+  Conditionnement: ${conditionnement}
+  Pourriez-vous m’envoyer un devis avec les prix et conditions ?
+  Merci.`;
+    
+    localStorage.setItem("demandeProduit", message);
+  }
+  
+  // Ce bloc s'exécutera dans index.html UNIQUEMENT !
+  document.addEventListener("DOMContentLoaded", function () {
+    const messageInput = document.getElementById("message-input");
+    const savedMessage = localStorage.getItem("demandeProduit");
+  
+    if (savedMessage && messageInput) {
+      messageInput.value = savedMessage;
+      localStorage.removeItem("demandeProduit");
+      messageInput.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+  
 // Function to copy phone number to clipboard
 function copyPhoneNumber() {
     const phoneNumber = "+213 661 77 40 79";
